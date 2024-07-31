81b721cd64803ea6b5dcbc905e62c339fb344885
nishigandhamali78 <nishigandhamali78@gmail.com>
2024-07-31 23:24:02 +0530

    datatype variable and typeofex
    datatype variable and typeofex

---
+++ 01-variableAssign.js
@@ -0,0 +1,48 @@
+
+console.log("=============1===========")
+
+var myFullName;
+console.log("My full Name before initilization", myFullName);
+
+var myFullName = "Nishigandha Mali";
+console.log("Initilaze the myFullName variable:",myFullName);
+
+var myFullName = "Nishigandha Rajendra Mali";
+console.log("update the  myFullName variable:",myFullName);
+
+console.log("=============2===========")
+
+var myLove="I Love only Js";
+console.log("Declare and initilaze the  myLove variable:",myLove);
+
+console.log("=============3===========")
+
+var firstName="Nishigandha ";
+console.log("My First Name:",firstName);
+
+var lastName="Mali";
+console.log("My Last Name:",lastName);
+
+var age=26;
+console.log("My age:",age);
+
+var address="mumbai";
+console.log("My Address:",address);
+
+var pinCode=400607;
+console.log("My pincode:",pinCode);
+
+var gender="Female";
+console.log("my gender",gender);
+
+var isMarried="No";
+console.log("isMarried:",isMarried);
+
+
+
+
+
+
+
+
+
---
+++ 02-typeOf.js
@@ -0,0 +1,39 @@
+console.log("=================1=============");
+
+var bankName;
+bankName="SBI Bank";
+console.log("Bank Name is:", bankName);
+var typeofbank=typeof bankName;
+console.log("type of bankName is:",typeofbank);
+
+console.log("=================2=============");
+
+var marks;
+marks=90;
+console.log("marks is:",marks);
+var typeofmarks=typeof marks;
+console.log("type of marks is:",typeofmarks);
+
+console.log("=================2=============");
+
+var isWorking;
+isWorking=true;
+console.log("isWorking:",isWorking);
+var typeofisWorking=typeof isWorking;
+console.log("type of isWorking is:",typeofisWorking);
+
+console.log("=================3=============");
+
+var totalCount="Hundred and seven";
+console.log("Total Count  is:",totalCount);
+var typeoftotalCount= typeof totalCount;
+console.log("tyeof Hundred and seven is :",typeoftotalCount);
+
+var totalCount=107;
+console.log(" updated Total Count  is:",totalCount);
+var typeoftotalCount= typeof totalCount;
+console.log("updated typeof Hundred and seven is :",typeoftotalCount);
+
+
+
+
