fbfb6c1ab3d1eb317397f824b0218582c078e025
nishigandhamali78 <nishigandhamali78@gmail.com>
2024-07-31 07:53:36 +0530

    javascript-variable
    javascript variable

---
+++ 01-variable-assignment.js
@@ -0,0 +1,12 @@
+console.log("Nishigandha Mali");
+cityName = "THANE"
+console.log("my name is",cityName);
+
+countryName = "INDIA"
+console.log("my name is",countryName);
+
+schoolName = "D.H.K  prashala"
+console.log("my name is",schoolName);
+
+ColgName = "SVERI College"
+console.log("my name is",ColgName);
