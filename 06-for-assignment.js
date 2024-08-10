39c4f866bd4372cc66657649fbc7280fa4d7e6cb
nishigandhamali78 <nishigandhamali78@gmail.com>
2024-08-10 13:05:39 +0530

    FOR LOOP
    FOR LOOP

---
+++ 06-for-assignment.js
@@ -0,0 +1,47 @@
+console.log("======WAP to print number from 5 to 15 by incrementing 1======= ");
+for (let index = 5; index <=15; index++) {
+    console.log(index);    
+}
+
+console.log("=======WAP to print numbers from 50 to 40. decrementing by 1=======");
+for (let index = 50; index >=40; index--) {
+    
+    console.log(index);   
+}
+console.log("=====WAP to find first 15 odd numbers====");
+for(var i=1;i<=15;i=i+2){
+    console.log(i);   
+}
+console.log("=====WAP to find first 10 even numbers====");
+for(var i=0;i<=10;i=i+2){
+    console.log(i);
+    
+}
+console.log("====WAP to print table of 5====");
+for(var i=5;i<=50;i=i+5){
+    console.log(i);
+    
+}
+console.log("=====WAP to print table of 10 ===");
+for(var i=10;i<=100;i=i+10){
+    console.log(i);
+    
+}
+console.log("=====WAP to print table of 10 in reverse order====");
+for(var i=100; i>=10;i=i-10){
+    console.log(i);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06-for-loop.js
@@ -0,0 +1,18 @@
+console.log(`======= Print even numbers from 0 to 10 ==========`);
+for (let index = 0; index <= 10; index=index+2) {
+    console.log(index);
+}
+
+console.log(`======= Print the table of 5 ==========`);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+}
+
+console.log(`======= Print the numbers from 50 to 40 ==========`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+console.log(`======= Print the table of 5 ==========`);
+for (let index = 90; index >= 9; index=index-9) {z
+    console.log(index);
+}
